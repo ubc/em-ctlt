28de6d315a8acedfa98a7dcb15cb0b2a8e9898cb
psmagicman <julienlaw1@gmail.com>
2013-03-04 16:17:26 -0800

    Now displays all categories with active events and the next upcoming event date

--- ctlt_espresso_category_display.php
+++ ctlt_espresso_category_display.php
@@ -11,12 +11,28 @@ Notes: This file should be stored in your "/wp-content/uploads/espresso/template
 */
 
 // registering scripts and styles here for sorting the table
-wp_register_script( 'ctlt_event_espresso_sort_table', trailingslashit(EVENT_ESPRESSO_UPLOAD_URL) . 'templates/js/espresso_sort_table.js', array('jquery')
+wp_register_script( 'ctlt_event_espresso_sort_table', trailingslashit(EVENT_ESPRESSO_UPLOAD_URL) . 'templates/js/espresso_sort_table.js', array('jquery'), '1.0', true );
+wp_register_script( 'ctlt_table_sorter_library', trailingslashit(EVENT_ESPRESSO_UPLOAD_URL) . 'templates/js/jquery.tablesorter.js', array('jquery'), '1.0', true );
+wp_register_style( 'ctlt_table_sorter_style', trailingslashit(EVENT_ESPRESSO_UPLOAD_URL) . 'templates/css/ctlt_event_espresso_list.css' );
+// add the above scripts and styles to the table so that the jQuery sorting function can be used
+wp_enqueue_script( 'ctlt_event_espresso_sort_table' );
+wp_enqueue_script( 'ctlt_table_sorter_library' );
+wp_enqueue_style( 'ctlt_table_sorter_style' );
 
 function ctlt_display_event_espresso_category(){
     global $wpdb;
-    $sql = "SELECT c.category_name, c.category_desc FROM "
-    . EVENTS_CATEGORY_TABLE . " c ";
+    $sql = "SELECT e.id, d.start_date, d.category_name, d.category_desc
+            FROM " . EVENTS_DETAIL_TABLE . " e
+            INNER JOIN
+            (
+                SELECT min(e.start_date) Start_date, c.category_name, c.category_desc
+                FROM " . EVENTS_DETAIL_TABLE . " e
+                INNER JOIN " . EVENTS_CATEGORY_REL_TABLE . " r ON r.event_id = e.id
+                INNER JOIN " . EVENTS_CATEGORY_TABLE . " c ON c.id = r.cat_id
+                GROUP BY c.category_name, c.category_desc
+)  d ON e.start_date = d.Start_date
+WHERE e.is_active = 'Y'
+ORDER BY date(e.start_date)";
 
     ctlt_event_espresso_get_category_list_table($sql);
 }
@@ -41,19 +57,23 @@ function ctlt_event_espresso_get_category_list_table($sql){
        // echo '<p>' . htmlspecialchars_decode($category_desc) . '</p>';
     // }
 ?>
-    <table class="table table-bordered table-hover table-condensed">
+    <table id="ctlt-jQuery-event-espresso-sort-table" class="table table-bordered table-hover table-condensed tablesorter">
         <thead>
             <tr>
-                <th class="header headerSortDown">Series</th>
-                <th>Description</th>
+                <th class="header headerSortDown">Date</th>
+                <th class="header">Series</th>
             </tr>
         </thead>
         <tbody>
 <?php
         foreach ($events as $event){
+            $event_id = $event->id;
+            $event_date = $event->start_date;
+            $cat_name = $event->category_name;
+            $cat_desc = $event->category_desc;
             echo '<tr>';
-                echo '<td>' . $event->category_name . '</td>';
-                echo '<td>' . $event->category_desc . '</td>';
+                echo '<td>' . event_date_display( $event_date, get_option( 'date_format' ) ) . '</td>';
+                echo '<td><a>' . $cat_name . '</a><br/>' . $cat_desc . '</td>';
             echo '</tr>';
         }
 ?>
@@ -61,3 +81,4 @@ function ctlt_event_espresso_get_category_list_table($sql){
     </table>
 <?php
 }
+
