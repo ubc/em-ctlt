ad22b9960d4934847c714fa57124fcef0b033a67
psmagicman <julienlaw1@gmail.com>
2013-04-29 12:03:24 -0700

    see v0.13.1 of the readme

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ events
 
 == changelog ==
 
+v0.13.1 - fixed a bug where an uncategorized event will not show up in the series view
+
 v0.13 - added multi-registration php files
       - made some UI changes to the event-list PHP files and the registration-page PHP files
 
--- ctlt_espresso_category_display.php
+++ ctlt_espresso_category_display.php
@@ -26,10 +26,10 @@ function ctlt_display_event_espresso_category($event_type){
     // if $event_type is identical to 'past' then assign '<' to $conditional otherwise '>'
     $conditional = ( $event_type === 'past' ) ? '<' : '>';
 
-    $sql = "SELECT e.id, min(e.start_date) AS start_date, c.category_name, c.category_desc, c.id AS cat_id, ese.start_time FROM " . EVENTS_DETAIL_TABLE . " e 
-            JOIN " . EVENTS_START_END_TABLE . " ese ON ese.event_id = e.id
-            JOIN " . EVENTS_CATEGORY_REL_TABLE . " r ON r.event_id = e.id
-            JOIN " . EVENTS_CATEGORY_TABLE . " c ON c.id = r.cat_id
+    $sql = "SELECT e.id, min(e.start_date) AS start_date, IFNULL(c.category_name, 'Uncategorized') AS category_name, c.category_desc, IFNULL(c.id, 0) AS cat_id, ese.start_time FROM " . EVENTS_DETAIL_TABLE . " e 
+            LEFT JOIN " . EVENTS_START_END_TABLE . " ese ON ese.event_id = e.id
+            LEFT JOIN " . EVENTS_CATEGORY_REL_TABLE . " r ON r.event_id = e.id
+            LEFT JOIN " . EVENTS_CATEGORY_TABLE . " c ON c.id = r.cat_id 
             WHERE e.is_active = 'Y'
             AND e.end_date " . $conditional . " CURDATE()
             GROUP BY c.category_name, c.category_desc, c.id
@@ -48,6 +48,7 @@ function ctlt_event_espresso_get_category_list_table($sql){
     global $wpdb;
     $events = $wpdb->get_results($sql);
     $num_rows = count($events);
+    //var_dump($events);
     $category_names = $wpdb->category_name;
     $category_descs = $wpdb->category_desc;
     
@@ -59,7 +60,7 @@ function ctlt_event_espresso_get_category_list_table($sql){
             $cat_name = $event->category_name;
             $cat_desc = $event->category_desc;
             $url = ctlt_espresso_category_url( $cat_id, $cat_name );
-
+            $description = ( $cat_id != 0 ) ? espresso_format_content( $cat_desc ) : espresso_format_content( "The events in here do not belong to any particular series." );
             ?>
             <div class="row-fluid">
                 <div class="span12" style="border: 1px solid #ccc; border-radius: 4px; margin-bottom: 10px;">
@@ -75,7 +76,7 @@ function ctlt_event_espresso_get_category_list_table($sql){
                                 <i class="icon-calendar"></i> <?php echo event_date_display( $event_date, get_option( 'date_format' ) ) ?>
                                 <br />
                                 <div class="event-desc">
-                                    <?php echo espresso_format_content( $cat_desc ); ?>
+                                    <?php echo $description; ?>
                                 </div>
                             </p>
                         </div>
@@ -91,7 +92,7 @@ function ctlt_event_espresso_get_category_list_table($sql){
 
 function ctlt_espresso_category_url( $cat_id = 0, $cat_name = NULL ) {
     global $org_options;
-    if ( $cat_id > 0 && !empty($cat_name) ) {
+    if ( $cat_id >= 0 && !empty($cat_name) ) {
         //return espresso_getTinyUrl(home_url().'/?page_id='.$org_options['event_page_id'].'&regevent_action=register&event_id='.$event_id);
         $new_url = add_query_arg('category_id', $cat_id, get_permalink() );
         $new_url = add_query_arg('category_name', $cat_name, $new_url );
